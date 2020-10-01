dd4b57b2bab876682467cd1733b3e5c31d599119
puppetagent <69377529+puppetagent@users.noreply.github.com>
2020-10-01 18:00:20 +0530

    Add files via upload

---
+++ papper.js
@@ -0,0 +1,35 @@
+class Papper {
+
+    constructor (x,y,height,width) {
+
+        var options ={
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+            this.body = Bodies.rectangle(x, y, 50 , 50, options);
+            this.width = 50;
+            this.height = 50;
+              
+            World.add(world, this.body);
+            }
+    display(){
+            var pos =this.body.position;
+//pos.x = mouseX;
+//pos.y = mouseY;
+            var angle = this.body.angle;
+            push();
+            translate(pos.x, pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            strokeWeight(3);
+            stroke ("blue");
+            fill ("red");
+            rect(0, 0, this.width, this.height);
+            pop();
+            }
+          
+}
+
+
---
+++ sketch.js
@@ -0,0 +1,56 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1600, 800);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	box1 = new Box (1140,770,200,10);
+  ground = new Ground(800,790,1600,20);
+  box2= new Box (1050,660,10,200);
+  box3 = new Box (1240,660,10,200);
+
+  papper = new Papper (100,700,20,20);
+
+  keyPressed();
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  Engine.update(engine);
+  rectMode(CENTER);
+  background(0);
+
+  box1.display();
+  ground.display();
+  box2.display();
+  box3.display();
+  papper.display();
+
+  drawSprites();
+ 
+}
+
+
+function keyPressed () {
+
+  if(keyCode === UP_ARROW) {
+
+      Matter.Body.applyForce (papper.body,papper.position,{ x : 190,y : -190 } );
+
+  }
+}
